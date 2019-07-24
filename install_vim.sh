e59b988e51a781e6141799a274b818826fbab209
Devin Neal <devin@eulertour.com>
2019-07-24 03:18:11 +0000

    vim updates

--- install_vim.sh
+++ install_vim.sh
@@ -9,28 +9,28 @@
 #   ./compile_full_vim.sh
 
 sudo apt remove -y \
-	vim \
-	vim-runtime \
-	gvim \
-	vim-tiny \
-	vim-common \
-	vim-gui-common
+    vim \
+    vim-runtime \
+    gvim \
+    vim-tiny \
+    vim-common \
+    vim-gui-common
 
 sudo apt install -y \
-	libncurses5-dev \
-	libgnome2-dev \
-	libgnomeui-dev \
-	libgtk2.0-dev \
-	libatk1.0-dev \
-	libbonoboui2-dev \
-	libcairo2-dev \
-	libx11-dev \
-	libxpm-dev \
-	libxt-dev \
-	python-dev \
-	python3-dev \
+    libncurses5-dev \
+    libgnome2-dev \
+    libgnomeui-dev \
+    libgtk2.0-dev \
+    libatk1.0-dev \
+    libbonoboui2-dev \
+    libcairo2-dev \
+    libx11-dev \
+    libxpm-dev \
+    libxt-dev \
+    python-dev \
+    python3-dev \
     ruby-dev \
-	git \
+    git \
     ripgrep
 
 cd ~/Code
@@ -41,16 +41,17 @@ cd fzf
 cd ~/Code
 git clone https://github.com/vim/vim.git
 cd vim
-./configure --with-features=huge \
-	--enable-multibyte \
-	--enable-rubyinterp \
-	--enable-python3interp \
+./configure \
+    --with-features=huge \
+    --enable-multibyte \
+    --enable-rubyinterp \
+    --enable-python3interp \
     --with-python3-config-dir=/usr/lib/python3.6/config-3.6m-x86_64-linux-gnu/ \
-	--enable-perlinterp \
-	--enable-luainterp \
-	--enable-gui=gtk2 \
-	--enable-cscope \
-	--prefix=/usr
+    --enable-perlinterp \
+    --enable-luainterp \
+    --enable-gui=gtk2 \
+    --enable-cscope \
+    --prefix=/usr
 make
 sudo make install
 
