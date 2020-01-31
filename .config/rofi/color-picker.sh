c550b1594b8057cdbeffa4d8989895fb153fc97b
Devin Neal <devin@eulertour.com>
2020-01-31 04:10:15 -0800

    Add rofi config

---
+++ .config/rofi/color-picker.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+BASE16_DIR="$HOME/github/base16-builder-python"
+BASE16_SCHEME_DIR="$BASE16_DIR/schemes"
+ALACRITTY_YML="$XDG_CONFIG_HOME/alacritty/alacritty.yml"
+
+if [ "$#" = 0 ]; then
+    for scheme_yaml in $(find "$BASE16_SCHEME_DIR" -name "*.yaml" | sort); do
+        printf "%s\n" "$(basename "$scheme_yaml" .yaml)"
+    done
+else
+    (cd "$BASE16_DIR" || exit 1; ./pybase16.py inject -s "$1" -f "$ALACRITTY_YML")
+fi
